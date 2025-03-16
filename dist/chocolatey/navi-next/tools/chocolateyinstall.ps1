6b80b1899212f13a75087561014b5a85378880a0
Alexis Opolka <53085471+alexis-opolka@users.noreply.github.com>
2025-03-16 15:03:40 +0100

    Next navi (#9)
    * Added the first chocolatey package and the start of the package script
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
    
    * Update navi-netx scripts
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
    
    * Updated nuspec
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
    
    * Updated the chocolatey package
    
    Still not working though
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
    
    * Update gitignore rules
    
    Ignore nupkg files
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
    
    * Update chocolatey dist
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>

---
+++ dist/chocolatey/navi-next/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helpers.ps1
+
+### Metadata related to the software and package
+$packageName = 'navi'
+$softwareName = 'navi-next'
+$version = '2.24.0-alpha1'
+
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version $version)
+$installPath = Get-ChocolateyPath -PathType 'PackagePath'
+
+Write-Output $installPath
+
+if ($alreadyInstalled -and !$env:ChocolateyForce) {
+  Write-Output $(
+  "navi-next is already installed. " +
+          'No need to download and re-install.'
+  )
+  exit
+}
+
+### We're checking to see if we have never installed navi before
+if (-Not(Test-Path $installPath)) {
+  New-Item -Path $installPath
+} else {
+  ### Otherwise, we delete possible old versions
+  Get-ChildItem $installPath\* | ? { $_.PSISContainer } | Remove-Item -Recurse -Force
+}
+
+# Download from an HTTPS location
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$toolsPath\$softwareName.zip" -Url "https://github.com/alexis-opolka/navi/releases/download/v$version/navi-v$version-x86_64-pc-windows-gnu.zip"
+Get-ChocolateyUnzip -FileFullPath "$toolsPath\$softwareName.zip" -Destination $toolsPath
+Install-ChocolateyPath -PathToInstall $toolsPath
